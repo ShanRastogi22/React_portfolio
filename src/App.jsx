8735f95334306d098be988ce023aca41356a74a0
ShanRastogi22 <nash82826@gmail.com>
2023-03-05 17:22:49 +0530

    Project started today

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './Header/Header'
+import Nav from './Nav/Nav'
+import About from './about/About'
+import Expeirienc from './Expeirience/Expeirience'
+import Services from './Services/Services'
+import Portfolio from './portfolio/Potfolio'
+import Testimonials from './Restimonials/Testimonials'
+import Contact from './contact/Contact'
+import Footer from './Footer/Footer' 
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Expeirienc />
+      <Services />
+      <Portfolio /> 
+      <Testimonials />
+      <Contact />
+      <Footer /> 
+    </>
+  )
+}
+
+export default App
---
+++ src/Expeirience/Expeirience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+const Expeirience = () => {
+  return (
+    <div>
+      Expeirience
+    </div>
+  )
+}
+
+export default Expeirience
